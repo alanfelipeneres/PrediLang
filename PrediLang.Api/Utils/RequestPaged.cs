1334253e79d8d2ff5cb2e457f9e6fc48690111c0
alanfelipeneres <alanfelipe.neres@outlook.com>
2024-06-23 18:51:57 -0300

    #40 - Criar o model genérico para receber requisições paginadas

--- PrediLang.Api/Utils/RequestPaged.cs
+++ PrediLang.Api/Utils/RequestPaged.cs
@@ -2,14 +2,12 @@
 
 namespace PrediLang.Api.Utils
 {
-    public class ResponsePaginado<T>
+    public class RequestPaged<T>
     {
-        public IEnumerable<T> data { get; set; }
+        public T data { get; set; }
         public int page { get; set; }
         public int totalPorPage { get; set; }
         public int total { get; set; }
         public string sortedBy { get; set; }
-        public bool success { get; set; }
-        public string message { get; set; }
     }
 }
