e83d5caf3fca21207ba3ae4bd74953b6d8536b6a
alanfelipeneres <alanfelipe.neres@outlook.com>
2024-06-23 18:48:19 -0300

    #40 - Criar o model genérico para retornar a paginação

---
+++ PrediLang.Api/Utils/ResponsePaginado.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace PrediLang.Api.Utils
+{
+    public class ResponsePaginado<T>
+    {
+        public IEnumerable<T> data { get; set; }
+        public int page { get; set; }
+        public int totalPorPage { get; set; }
+        public int total { get; set; }
+        public string sortedBy { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+}
